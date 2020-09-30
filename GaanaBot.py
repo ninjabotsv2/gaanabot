e87cbbb85f957eab6438e69a506902a70a8e270c
ninjabotsv2 <72125823+ninjabotsv2@users.noreply.github.com>
2020-09-30 13:00:16 +0530

    Update GaanaBot.py

--- GaanaBot.py
+++ GaanaBot.py
@@ -21,7 +21,7 @@ songdata_file_path = database_path + 'songdata.json'
 searchdata_file_path = database_path + 'searchdata.json'
 
 #Telegram bot token ID
-TOKEN = '###############################################'
+TOKEN = '1241164429:AAHxVZyysrHM8L9sE1p4-svlabRPxod38zA'
 
 #Base url for fetching json object 
 BASE_URL = 'http://www.youtubeinmp3.com/fetch/?format=JSON&video='
